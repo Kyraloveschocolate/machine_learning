2aba83bf6c0e48569dd42fe2eb9a50fa152a405d
Kyraloveschocolate <45490700+Kyraloveschocolate@users.noreply.github.com>
2021-01-09 20:56:30 +0100

    Add files via upload
    als t goed is is dit de eindopdracht van speltheorie fundament

---
+++ start bke.py
@@ -0,0 +1,15 @@
+from bke import *
+
+
+class MijnSpeler(EvaluationAgent):
+    def evaluate(self, board, my_symbol, opponent_symbol):
+        getal = 1
+        if can_win(board, opponent_symbol):
+            getal = getal - 1000
+        return getal
+        if board[4] == my_symbol:
+            getal = getal + 5
+        return getal
+    
+mijn_speler = MijnSpeler()
+start(player_o=mijn_speler)
