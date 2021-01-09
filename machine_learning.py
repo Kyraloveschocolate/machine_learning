9f842b95e89b74b3c221c01320a953fc2c2c358d
Kyraloveschocolate <45490700+Kyraloveschocolate@users.noreply.github.com>
2021-01-09 21:33:14 +0100

    Add files via upload
    Beste prestatie , machine learning fundament

--- machine_learning.py
+++ machine_learning.py
@@ -15,12 +15,12 @@ class MyAgent(MLAgent):
     
     
 random.seed(1)
-my_agent = MyAgent(alpha=0.5, epsilon=0.2)
+my_agent = MyAgent(alpha=0.2, epsilon=0.7)
 random_agent = RandomAgent()
 
 train_and_plot(
     agent=my_agent,
     validation_agent=random_agent,
     iterations=70,
-    trainings=400,
-    validations=6000)
+    trainings=200,
+    validations=1000)
